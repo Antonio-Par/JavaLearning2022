fde7433acd29c5aff2bd6a738dab846eedd456ab
Antonio-Par <polikarpez@gmail.com>
2022-02-12 19:49:04 +0300

    Add days 1-4 (added forgotten tasks)

---
+++ src/main/java/day1/Task5.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        for (int olymp_year = 1980; olymp_year <= 2020; olymp_year += 4) {
+            System.out.println("Олимпиада "+olymp_year+" года");
+        }
+    }
+}
