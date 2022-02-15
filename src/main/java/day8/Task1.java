55ed66f5ace9fa286f0f59acb41865b70646aaa1
Antonio-Par <polikarpez@gmail.com>
2022-02-15 23:04:44 +0300

    Day 7 has added

---
+++ src/main/java/day8/Task1.java
@@ -0,0 +1,31 @@
+package day8;
+
+public class Task1 {
+        public static void main(String[] args) {
+            int number = 0;
+            long durationStr, durationStrBld;
+            StringBuilder strBld = new StringBuilder("0");
+            String str = "0";
+
+            for (int i = 1; i <= 20000; i++) {
+                str += " " + String.valueOf(i);
+            }
+            long startTime = System.currentTimeMillis();
+            System.out.println(str);
+            long stopTime = System.currentTimeMillis();
+            durationStr = stopTime - startTime;
+            System.out.println("Output procedure duration is " + durationStr + " ms");
+
+            for (int i = 1; i <= 20000; i++) {
+                strBld.append(" " + i);
+            }
+            startTime = System.currentTimeMillis();
+            System.out.println(strBld.toString());
+            stopTime = System.currentTimeMillis();
+            durationStrBld = stopTime - startTime;
+            System.out.println("Output procedure duration is " + durationStrBld + " ms");
+
+            System.out.println("Duration of StringBuilder output is " + durationStrBld + " ms versus " + durationStr + " ms of String outupt");
+
+        }
+}
---
+++ src/main/java/day8/Task2.java
@@ -0,0 +1,8 @@
+package day8;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Airplane airplane = new Airplane("Boeing", 2000, 150.47f, 10000.55f);
+        System.out.println(airplane);
+    }
+}
